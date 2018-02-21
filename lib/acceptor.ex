dd88787af36a9266c6722c2661ff6bbbc701da4a
Emanuele Rossi <er1115@imperial.ac.uk>
2018-02-21 17:19:13 +0000

    State reduction optimisation for acceptors

--- lib/acceptor.ex
+++ lib/acceptor.ex
@@ -1,21 +1,22 @@
 defmodule Acceptor do
   def start _config do
     # The ballot number { integer, leader } must follow total ordering
-    next {0, 0}, MapSet.new
+    next {0, 0}, Map.new
   end
 
-  defp put_if_true(condition, set, elem) do
-    if condition, do: (MapSet.put set, elem), else: set
+  defp put_if_true(condition, map, key, elem) do
+    if condition, do: Map.put(map, key, elem), else: map
   end
 
   defp next ballot_num, accepted do
     receive do
       {:prepare, leader, leader_ballot_num} ->
         ballot_num = max ballot_num, leader_ballot_num
-        send leader, {:promise, ballot_num, accepted}
+	accepted_set = accepted |> Map.values |> MapSet.new
+        send leader, {:promise, ballot_num, accepted_set}
         next ballot_num, accepted
-      {:accept, leader, {leader_ballot_num, _slot_num, _cmd}=pvalue} ->
-        accepted = put_if_true ballot_num == leader_ballot_num, accepted, pvalue
+      {:accept, leader, {leader_ballot_num, slot_num, _cmd}=pvalue} ->
+        accepted = put_if_true ballot_num == leader_ballot_num, accepted, slot_num, pvalue
         send leader, {:accepted, ballot_num, {}}
         next ballot_num, accepted
     end
